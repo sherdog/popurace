65e801113d98fbcd6017f181140693aefffc2999
sherdog <sherdog@gmail.com>
2019-06-17 22:45:46 -0500

    now with homepage login

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const path = require('path');
 const url = require('url');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'pug')
 app.set('views', __dirname + '/views');
+app.set('view options', { pretty: true });
 
 app.get('/', function(req, res){
+	res.render('index', { host: req.headers.host });
+});
+
+app.get('/community', function(req, res){
 	res.render('communities/home', { host: req.headers.host });
 });
 
@@ -30,7 +37,7 @@ io.sockets.on('connection', function(socket){
 	});
 });
 
-let port = 8080;
+let port = 3000;
 if (process.env.PORT)
 	port = process.env.PORT;
 

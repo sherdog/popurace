1c8c3600d459fa8f156946b12e785c0a109009e8
Mike Sheridan <sherdog@gmail.com>
2019-06-26 18:48:32 -0500

    more organization

--- app/index.js
+++ app/index.js
@@ -7,13 +7,12 @@ const config = require('./config');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use('/static', express.static(path.resolve('public')))
-app.set('view options', { pretty: true })
-app.set('views', path.join(__dirname, 'components'))
-app.set('view engine', 'pug')
-console.log('Using hash: ' + config.session.hash)
-app.use(session({ secret: config.session.hash, cookie: { maxAge: 60000 }}))
+app.use('/static', express.static(path.resolve('public')));
+app.set('view options', { pretty: true });
+app.set('views', path.join(__dirname, 'components'));
+app.set('view engine', 'pug');
+app.use(session({ secret: config.session.hash, cookie: { maxAge: 60000 }}));
 
-app.use(require('./routes'))
+app.use(require('./routes'));
 
 module.exports = app
